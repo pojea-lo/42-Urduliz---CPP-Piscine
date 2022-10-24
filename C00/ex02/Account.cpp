2b0c4373974c948da2cf70d70d0b92696d019c9e
Pablo Ojea Lopez <pyoyoso@gmail.com>
2022-10-24 12:08:43 +0200

    Commit 01 - C00 terminado

--- C00/ex02/Account.cpp
+++ C00/ex02/Account.cpp
@@ -22,7 +22,11 @@ Account::Account ( int initial_deposit ) {
 	Account::_nbAccounts++;
 	this->_amount = initial_deposit;
 	Account::_totalAmount += this->_amount;
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	std::cout << "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";created" << std::endl;
 
 	return;
 }
@@ -30,18 +34,115 @@ Account::Account ( int initial_deposit ) {
 //Destructor
 Account::~Account (void) {
 
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";closed" << std::endl;
+
 	return;
+}
+
+//Getters
+int	Account::getNbAccounts( void ) {
+	
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+	
+	return Account::_totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+	
+	return Account::_totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void ) {
+	
+	return Account::_totalNbWithdrawals;
+}
+
+//"Constructor" de depósitos
+void	Account::makeDeposit( int deposit) {
+
+	int	p_amount;
+
+	this->_nbDeposits += 1;
+	Account::_totalNbDeposits += 1;
+	p_amount = this->_amount;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";p_amount:" << p_amount
+		<< ";deposit:" << deposit 
+		<< ";amount:" << this->_amount
+		<< ";nb_depposits:" << this->_nbDeposits
+		<< std::endl;
+
+	return;
+}
+
+//"Constructor" de withdrawals
+bool	Account::makeWithdrawal( int drawal) {
+
+	int	p_amount;
+
+	p_amount = this->_amount;
+	if (drawal <= p_amount) {
+		
+		this->_nbWithdrawals += 1;
+		Account::_totalNbWithdrawals += 1;
+		this->_amount -= drawal;
+		Account::_totalAmount -= drawal;
+		
+		Account::_displayTimestamp();
+		std::cout << "index:" << this->_accountIndex
+			<< ";p_amount:" << p_amount
+			<< ";withdrawal:" << drawal
+			<< ";amount:" << this->_amount
+			<< ";nb_withdrawals:" << this->_nbWithdrawals
+			<< std::endl;
+	}
+	else {
+	
+		Account::_displayTimestamp();
+		std::cout << "index:" << this->_accountIndex
+			<< ";p_amount:" << this->_amount
+			<< ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+
+	return (true);
 }
 
 //metodo para mostrar totales
 void	Account::displayAccountsInfos( void ) {
 
-	std::cout << "accounts:" << Account::_nbAccounts << ";total:" << Account::_totalAmount << std::endl;
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::_nbAccounts
+		<< ";total:" << Account::_totalAmount 
+		<< ";deposits:" << Account::_totalNbDeposits 
+		<< ";withdrawals:" << Account::_totalNbWithdrawals 
+		<< std::endl;
 
 	return;
 }
 
-//
+//metodo para mostrar datos al inicio
+void	Account::displayStatus( void ) const {
+
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";deposits:" << this->_nbDeposits
+		<< ";withdrawals:" << this->_nbWithdrawals
+		<< std::endl;
+		
+	return;
+}
 
 //metodo para mostrar el tiempo
 void	Account::_displayTimestamp( void ) {
@@ -69,5 +170,3 @@ void	Account::_displayTimestamp( void ) {
 
 	return;
 }
-
-
