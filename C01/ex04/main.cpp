68d0c452a40e0f6be4224123ab3ba2163991d27c
Pablo Ojea Lopez <pyoyoso@gmail.com>
2022-11-02 08:02:27 +0100

    Comit 08 - C01-04 started

---
+++ C01/ex04/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int	main(void) {
+
+	std::string		resp = "";
+	std::string		s1 = "";
+	std::string		s2 = "";
+
+	std::cout << "Please type file name to work: ";
+	std::cin >> resp;
+
+
+
+
+	return 0;
+}
