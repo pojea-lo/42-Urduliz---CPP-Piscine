69c6adc08005a2e1a21e0cce5f0c118385bceb4e
Pablo Ojea Lopez <pyoyoso@gmail.com>
2022-11-05 08:00:53 +0100

    Commit 11 - C01-06 Started

---
+++ C01/ex06/Karen.cpp
@@ -0,0 +1,67 @@
+#include "Karen.hpp"
+
+#include <iostream>
+
+//constructor and destructor
+Karen::Karen() {
+
+	return;
+}
+
+Karen::~Karen() {
+
+	return;
+}
+
+//privates functions
+void	Karen::debug() {
+
+	std::cout << "DEBUG - I love Karen but..." << std::endl;
+	return;
+}
+
+void	Karen::info() {
+
+	std::cout << "INFO - I love Karen but sometimes..." << std::endl;
+	return;
+}
+
+void	Karen::warning() {
+
+	std::cout << "WARN - Maybe Karen is not so good!!!" << std::endl;
+	return;
+}
+
+void	Karen::error() {
+
+	std::cout << "ERNO - Fucking Karen, always the same" << std::endl;
+	return;
+}
+
+//methods
+void	Karen::complain(std::string level) {
+
+	std::string		list[4] = {"debug", "info", "warning", "error"};
+	int				i = 0;
+	void			(Karen::*ptrFun)(void) = NULL;
+	
+	while (level.compare(list[i]) != 0 && i < 4)
+		i++;
+	
+	switch (i) {
+		case 0:
+			ptrFun = &Karen::debug;
+			break;
+		case 1:
+			ptrFun = &Karen::info;
+			break;
+		case 2:
+			ptrFun = &Karen::warning;
+			break;
+		case 3:
+			ptrFun = &Karen::error;
+			break;
+	}
+	(this->*ptrFun)();
+	return;
+}
---
+++ C01/ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Karen.hpp"
+
+int	main(int argv, char argc[]) {
+
+	Karen			KKaren;
+	std::string		level[4] = 
+
+	if (argv != 2)
+		std::cout << "Bad number of arguments!!" << std::endl;
+	KKaren::complain(argc[1]);	
+	return 0;
+}
