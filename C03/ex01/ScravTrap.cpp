07835b80978d5c47133d7ab0ddc17c27e6146f78
Pablo Ojea Lopez <pyoyoso@gmail.com>
2022-11-23 06:32:14 +0100

    Commit - C03-01 Done & C03-00 Modified

---
+++ C03/ex01/ScravTrap.cpp
@@ -0,0 +1,33 @@
+#include "ScravTrap.hpp"
+
+//constructor & destructor
+ScravTrap::ScravTrap() {
+
+	std::cout << "Empty ScravTrap constructor was called" << std::endl;
+	return;
+}
+
+ScravTrap::ScravTrap(std::string _Name): ClapTrap(_Name) {
+
+	std::cout << "Parameter ScravTrap constructor was called" << std::endl;
+	return;
+}
+
+ScravTrap::ScravTrap(const ScravTrap &obj): ClapTrap(obj) {
+	
+	std::cout << "ScravTrap copy constructor was called" << std::endl;
+	return;
+}
+
+ScravTrap::~ScravTrap() {
+
+	std::cout << "ScravTrap destructor was called" << std::endl;
+	return;
+}
+
+//member functions
+void	ScravTrap::guardGate() {
+
+	std::cout << getName() << " has enterred in Gate keeper mode." << std::endl;
+	return;
+}
