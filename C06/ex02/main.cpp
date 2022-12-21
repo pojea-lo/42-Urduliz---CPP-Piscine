0449adbf1f6e6d1ff71207a307e6cf700b6854e0
pojea-lo <pojea-lo@student.42urduliz.com>
2022-12-21 17:04:21 +0100

    Commit - C06-01 revised

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -7,14 +7,23 @@
 
 Base	*generate(void);
 void	identify(Base *p);
+void	identify(Base &p);
 
 int	main() {
 
 	Base	*one;
+	Base	*two = NULL;
 
 	one = generate();
+
 	
-	identify (one);
+		std::cout << "La clase de one es:           ";
+		identify (one);
+		identify (*one);
+		std::cout << "La clase de two (null) es:    ";
+		identify (two);
+		identify (*two);
+	
 
 	delete one;
 	return 0;
@@ -46,6 +55,9 @@ Base	*generate(void) {
 
 void	identify(Base *p) {
 
+	if (!p)
+		std::cout << "null" << std::endl;
+
 	A	*a = dynamic_cast<A*> (p);
 	if (a)
 		std::cout << "A" << std::endl;
@@ -57,5 +69,52 @@ void	identify(Base *p) {
 	C	*c = dynamic_cast<C*> (p);
 	if (c)
 		std::cout << "C" << std::endl;
+	
+	return;
+}
+
+void	identify(Base &p) {
+
+	try {
+		if (&p == (void *)0x0)
+			throw std::bad_cast();
+	}
+	catch (std::exception &ex) {
+
+		std::cout << "Casted of &null is not possible" << std::endl;
+		return;
+	}
+
+	try {
+		A &a = dynamic_cast<A&> (p);
+		(void) a;
+		std::cout << "&A" << std::endl;
+
+	}
+	catch (std::exception &ex) {
+
+		std::cout << "Casted &A is not possible" << std::endl;
+	}
+	try {
+		B &b = dynamic_cast<B&> (p);
+		(void) b;
+		std::cout << "&B" << std::endl;
+
+	}
+	catch (std::exception &ex) {
+
+		std::cout << "Casted &B is not possible" << std::endl;
+	}
+	try {
+		C &c = dynamic_cast<C&> (p);
+		(void) c;
+		std::cout << "&C" << std::endl;
+
+	}
+	catch (std::exception &ex) {
+
+		std::cout << "Casted &C is not possible" << std::endl;
+	}
+
 	return;
 }
