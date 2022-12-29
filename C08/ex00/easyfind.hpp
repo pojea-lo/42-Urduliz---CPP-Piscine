0e65a0efae5964feace1982a217526327c79abbe
pojea-lo <pojea-lo@student.42urduliz.com>
2022-12-29 22:33:33 +0100

    q

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -2,13 +2,14 @@
 # define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
 
 template <class T>
 
-void    easyfind(const T &t, const int &n) {
+void    easyfind(const std::vector<T> &t, const int &n) {
 
     try {
-        for (size_t i = 0; i < (sizeof(t) / sizeof(int)); i++) {
+        for (size_t i = 0; i < t.size(); i++) {
             if (t[i] == n) {
                 std::cout << "In the " << i << " possition we have the firs occurrence of " << n << std::endl;
                 return;
--- C08/ex00/main.cpp
+++ C08/ex00/main.cpp
@@ -2,14 +2,15 @@
 
 int     main() {
 
-    int     vector[5] = {1,2,3,4,5};
-    int     n1 = 3;
-    int     n2 = 7;
-    int     n3 = 5;
+    std::vector<int>     vector(5);
+    vector = {1,2,3,4,5};
+    int                  n1 = 3;
+    int                  n2 = 7;
+    int                  n3 = 5;
 
     std::cout << "The vector of ints is: ";
-    for (size_t i = 0; i < sizeof(vector) / sizeof(int); i++)
-        std::cout << vector [i] << "   ";
+    for (size_t i = 0; i < vector.size(); i++)
+        std::cout << vector[i] << "   ";
     std::cout << std::endl;
 
     std::cout << "\nTry to search: " << n1 << std::endl;;
