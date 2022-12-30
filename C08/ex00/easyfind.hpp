1f92898fe32282889f94b0107f62df139bca150e
pojea-lo <pojea-lo@student.42urduliz.com>
2022-12-30 10:22:26 +0100

    C08 - Finished

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -3,19 +3,18 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 template <class T>
 
 void    easyfind(const std::vector<T> &t, const int &n) {
 
     try {
-        for (size_t i = 0; i < t.size(); i++) {
-            if (t[i] == n) {
-                std::cout << "In the " << i << " possition we have the firs occurrence of " << n << std::endl;
-                return;
-            }
-        }
-        throw 1;
+        auto pos = std::find(t.begin(), t.end(), n);
+        if (pos != t.end())   
+            std::cout << "Yes" << std::endl;
+        else
+            throw 1;
     }
     catch (int ex) {
         std::cout << "No coincidence!!" << std::endl;
