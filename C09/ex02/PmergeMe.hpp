d783a434c600bbb9a26258b8942e8c3b925e0257
Pablo Ojea Lopez <pyoyoso@gmail.com>
2023-04-14 16:59:29 +0200

    C09-ex02 a falta de templar la clase entera, pero algoritmo funcionando

--- C09/ex02/PmergeMe.hpp
+++ C09/ex02/PmergeMe.hpp
@@ -26,7 +26,42 @@ class PmergeMe {
 		std::vector<int>		getVector();
 		std::string				getVec();
 
-		void			orden(std::vector<int> container);
+		template <typename T>
+		void			orden(T container) {
+
+			std::set<int>	aux1;
+			std::set<int>	aux2;
+
+			for (size_t i = 0; i < (container.size() / 2); i++)
+				aux1.insert(container[i]);
+			for (size_t i = (container.size() / 2); i < container.size(); i++)
+				aux2.insert(container[i]);
+
+			while (!container.empty())
+				container.pop_back();
+
+			while (!aux1.empty()) {
+
+				std::set<int>::iterator it1 = aux1.begin();
+				if (!aux2.empty()) {
+
+					std::set<int>::iterator it2 = aux2.begin();
+					if (*it1 < *it2) {
+						container.push_back(*it1);
+						aux1.erase(it1);
+					}
+					else {
+						container.push_back(*it2);
+						aux2.erase(it2);
+					}
+				}
+				else {
+					container.push_back(*it1);
+					aux1.erase(it1);
+				}
+			}
+			return;
+		}
 
 };
 
